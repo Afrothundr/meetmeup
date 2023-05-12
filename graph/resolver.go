eefb4611c9257fe10f6b0de828265a92113f7dbf
branford <branford.harris@adhocteam.us>
2023-05-12 15:13:15 -0500

    init

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/afrothundr/meetmeup/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	meetups []*model.Meetup
+}
